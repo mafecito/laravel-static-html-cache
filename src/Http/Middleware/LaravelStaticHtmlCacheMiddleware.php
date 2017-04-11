a177d7646113cb0ea34503034407bd405c936a41
Michael Scharl <michael.scharl@me.com>
2017-04-11 12:07:45 +0200

    Refactor cache logic

--- src/Http/Middleware/LaravelStaticHtmlCacheMiddleware.php
+++ src/Http/Middleware/LaravelStaticHtmlCacheMiddleware.php
@@ -13,9 +13,20 @@ class LaravelStaticHtmlCacheMiddleware
      */
     private $files;
 
+    /**
+     * @var bool
+     */
+    private $enabled;
+
     function __construct(Filesystem $files)
     {
         $this->files = $files;
+
+        if(config('static-html-cache.enabled') === 'debug') {
+            $this->enabled = !config('app.debug');
+        } else {
+            $this->enabled = config('static-html-cache.enabled');
+        }
     }
 
     /**
@@ -32,19 +43,52 @@ class LaravelStaticHtmlCacheMiddleware
 
     public function terminate(\Illuminate\Http\Request $request, \Illuminate\Http\Response $response)
     {
-        if ($response->isOk()) {
-            dd($request);
-            $filename = $request->route('slug');
-            $filename = public_path(config('static-html-cache.cache_path_prefix') . "/" . $filename);
+        if ($this->shouldStoreResponse($request, $response)) {
+            $filename = $this->getFilename($request);
+
+            $this->ensureStorageDirectory($filename);
 
             $file = $response->getContent();
-            $path = dirname($filename);
-
-            if (!$this->files->isDirectory($path)) {
-                $this->files->makeDirectory($path, 0777, true);
-            }
 
             $this->files->put($filename, $file);
+        }
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Response $response
+     * @return bool
+     */
+    private function shouldStoreResponse(\Illuminate\Http\Request $request, \Illuminate\Http\Response $response)
+    {
+        $isGETRequest = $request->getMethod() === 'GET';
+        $hasNoParams = count($request->input()) === 0;
+        $isHtmlMimeType = strpos($response->headers->get('content-type'), 'text/html') !== false;
+
+        return $this->enabled && $response->isOk() && $isGETRequest && $hasNoParams && $isHtmlMimeType;
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return mixed
+     */
+    private function getFilename(\Illuminate\Http\Request $request)
+    {
+        $request_uri = trim($request->getRequestUri(), '/');
+        $request_uri = empty($request_uri) ? '' : '/' . $request_uri;
+        $filename = public_path(config('static-html-cache.cache_path_prefix') . $request_uri . "/index.html");
+        return $filename;
+    }
+
+    /**
+     * @param $filename
+     */
+    private function ensureStorageDirectory($filename)
+    {
+        $path = dirname($filename);
+
+        if (!$this->files->isDirectory($path)) {
+            $this->files->makeDirectory($path, 0777, true);
         }
     }
 }
